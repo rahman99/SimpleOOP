9fe776ffd41435ca0ff1e24ad07d49790fa46322
abdurrahman <abdurrahman.man@gmail.com>
2014-07-19 21:24:33 +0700

    menambah convert number to word

--- src/rahman/sample/oop/SampleOOP.java
+++ src/rahman/sample/oop/SampleOOP.java
@@ -1,24 +1,56 @@
 package rahman.sample.oop;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class SampleOOP {
 
 	public static void main(String[] args) {
+		//Convert Integer To Word Numbering
+		System.out.println("=============Convert Number to word========");
+		ConvertNumberToWord numToWord = new ConvertNumberToWord();
+		int inputNumberToWord = 982723341;
+		
+		//get USD Currency Format
+		//NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(Locale.US);
+		System.out.println( convertNumberToRupiahFormat(inputNumberToWord) + " = " + numToWord.convertNumberToWord(inputNumberToWord));
+		
 		// Convert Integer to RomanNumber
-		ConvertRomanNumber roman = new ConvertRomanNumber();
-		int input=501;
+		System.out.println("\n\n=============Convert Number Roman Numbering========");
+		ConvertNumberToRoman roman = new ConvertNumberToRoman();
+		int input=511;
 		System.out.println(input + " --> "+ roman.RomanNumerals(input));
 		
+		
+		System.out.println("\n\n=============Reverse Number and Polindrome========");
 		LogicalGame logicGame = new LogicalGame();
+		//===========Reverse Number=========================
 		logicGame.reverseNumber(12930);
 		
+		//===========Detect Polindrome======================
 		logicGame.polindrome("bohoohob");
 		
+		//=============get Recursive Manual=================
 		logicGame.recursiveManual(1,4);
 		System.out.println("\n ========");
 		
-		logicGame.getStarTriangle(9);
+		//===============Drawing Triangle Number============
+		logicGame.getStarTriangle(4);
+		
 	}
 	
 	
+	
+	
+	
+	
+	public static String convertNumberToRupiahFormat(int nominal){
+		Locale locale = new Locale("ca", "CA");
+//		Locale locale = new Locale("rp", "RP");
+		NumberFormat rupiahFormat = NumberFormat.getCurrencyInstance(locale);
+		String rupiah = rupiahFormat.format(Double.parseDouble(String.valueOf(nominal))).substring(4);
+		
+		return "Rp "+rupiah;
+	}
 
 }
